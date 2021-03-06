ba4e8ff0b2b3d1ecdf2542ddbdee76b8586216bf
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-03-06 15:35:14 +0100

    GET unserialized data showing pizza's names ect..

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import requests
+import  json
 
 url = 'http://127.0.0.1:8000/api/GetPizza'
 
 data = requests.get(url)
-print(data.text)
+# print(data.text)
 
+pizzas = json.loads(data.text)
+print(pizzas)
+print(len(pizzas))
+
+# Show pizza's names
+for pizza in pizzas:
+    pizzaModel = pizza['fields']
+    print(pizzaModel)
 
 # Press "RUN" to see serialize data in venv console
